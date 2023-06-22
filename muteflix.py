f93520114d1e609fefef6b75ba7d679d9095a4f0
Velcorn <Velcorn@users.noreply.github.com>
2023-06-22 23:40:03 +0200

    Added simple GUI to start/stop script and display mute status Added pyinstaller to build an executable TODO: Add README

--- muteflix.py
+++ muteflix.py
@@ -1,52 +1,139 @@
-import cv2  # for debugging purposes
+import cv2
 import numpy as np
 import pytesseract
+import tkinter as tk
 from mss import mss
 from time import sleep
+import threading
+import pythoncom
 
 # Handle different OSes
 from sys import platform
+
 if platform == "win32":
-    from pycaw.pycaw import AudioUtilities, ISimpleAudioVolume
+    from pycaw.pycaw import AudioUtilities
 elif platform == "darwin":
     import osascript
 else:
-    raise Exception("Unsupported OS")
+    import subprocess
 
-# Bounding box for Netflix ad
-# TODO: Make this dynamic based on screen size
-bounding_box = {'top': 0, 'left': 1270, 'width': 140, 'height': 100}
+# Bounding box for Netflix ad, adjust to screen size
+bbox = {'top': 30, 'left': 1720, 'width': 200, 'height': 40}
+root = tk.Tk()
+width, height = root.winfo_screenwidth(), root.winfo_screenheight()
+ratios = width // 1920, height // 1080
+bbox['top'] *= ratios[1]
+bbox['left'] *= ratios[0]
+bbox['width'] *= ratios[0]
+bbox['height'] *= ratios[1]
 
-# Main loop
-sct = mss()
-mute = False
-while True:
-    img = np.array(sct.grab(bounding_box))
-    text = pytesseract.image_to_string(img, config='--psm 3')
-    # If the text contains 'ad' or a number, mute the system
-    if 'ad' in text.lower() or any(i.isdigit() for i in text):
-        if not mute:
-            print("Muting")
-            mute = True
-            if platform == "win32":
-                sessions = AudioUtilities.GetAllSessions()
-                for session in sessions:
-                    volume = session._ctl.QueryInterface(ISimpleAudioVolume)
-                    volume.SetMute(1, None)
-            else:
-                osascript.osascript("set volume output muted TRUE")
-        else:
-            sleep(0.5)
+muted = False  # Initial state
+is_running = False  # Flag to control script execution
+
+
+def mute():
+    global muted
+    if platform == "win32":
+        sessions = AudioUtilities.GetAllSessions()
+        for session in sessions:
+            if session.Process and any(s in session.Process.name() for s in ['firefox', 'chrome']):
+                volume = session.SimpleAudioVolume
+                volume.SetMute(1, None)
+        print("Muted")
+    elif platform == "darwin":
+        osascript.osascript("set volume output muted TRUE")
     else:
-        if mute:
-            print("Unmuting")
-            mute = False
-            if platform == "win32":
-                sessions = AudioUtilities.GetAllSessions()
-                for session in sessions:
-                    volume = session._ctl.QueryInterface(ISimpleAudioVolume)
-                    volume.SetMute(0, None)
+        subprocess.run(["amixer", "-D", "pulse", "sset", "Master", "mute"])
+    muted = True
+    update_status_label()
+
+
+def unmute():
+    global muted
+    if platform == "win32":
+        sessions = AudioUtilities.GetAllSessions()
+        for session in sessions:
+            if session.Process and any(s in session.Process.name() for s in ['firefox', 'chrome']):
+                volume = session.SimpleAudioVolume
+                volume.SetMute(0, None)
+    elif platform == "darwin":
+        osascript.osascript("set volume output muted FALSE")
+    else:
+        subprocess.run(["amixer", "-D", "pulse", "sset", "Master", "unmute"])
+    muted = False
+    update_status_label()
+
+
+def update_status_label():
+    if muted:
+        status_label.config(text="Muted", fg="red")
+    else:
+        status_label.config(text="Unmuted", fg="green")
+
+
+def toggle_mute():
+    if muted:
+        unmute()
+    else:
+        mute()
+
+
+def toggle_script_execution():
+    global is_running
+    is_running = not is_running
+    if is_running:
+        start_stop_button.config(text="Stop")
+        script_thread = threading.Thread(target=run_script_wrapper)
+        script_thread.daemon = True
+        script_thread.start()
+    else:
+        start_stop_button.config(text="Start")
+
+
+# GUI setup
+root.title("MuteflixPy")
+root.geometry("300x100")  # Initial window size
+root.resizable(True, True)  # Make window resizable
+start_stop_button = tk.Button(root, text="Start", command=toggle_script_execution)
+start_stop_button.pack(pady=10)
+status_label = tk.Label(root, text="Unmuted", fg="green")
+status_label.pack()
+
+
+# Main script loop wrapper
+def run_script_wrapper():
+    # Initialize the COM library
+    pythoncom.CoInitialize()
+    run_script()
+
+
+# Main script loop
+def run_script():
+    while is_running:
+        sct = mss()
+        img = np.array(sct.grab(bbox))
+
+        # Debugging
+        """cv2.imshow('test', img)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()"""
+
+        # If the text contains 'ad' or a number, mute the system
+        text = pytesseract.image_to_string(img, config='--psm 3')
+        if any(i.isdigit() for i in text):
+            if not muted:
+                mute()
+                sleep(int(''.join(filter(str.isdigit, text))))
+        elif 'ad' in text.lower():
+            if not muted:
+                mute()
             else:
-                osascript.osascript("set volume output muted FALSE")
+                sleep(0.5)
         else:
-            sleep(0.5)
+            if muted:
+                unmute()
+            else:
+                sleep(0.5)
+
+
+root.mainloop()
